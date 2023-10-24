967cc2f6da35861705b723d3a3c00e71ea5fdd7c
Dr. Dillinger <peter.dillinger@srh.de>
2023-10-24 09:51:05 +0200

    while-Schleife: keine Endlosschleife, da Ganzzahlen einen Überlauf haben!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         /* für den Anfang wird aller Programmcode hier in "main" stehen */
-        
+
+        short x = 10;
+        while ( x > 0 ) {
+            System.out.println("Hallo, wir sind bei " + x);
+            x = (short)(x + 1);
+        }
     }
 }
